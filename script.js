f5a1316524758462a40deed2c87b8159d163336a
Luiz <luikznn364@gmail.com>
2026-03-11 16:36:36 -0300

    Adição dos arquivos principais

---
+++ script.js
@@ -0,0 +1,26 @@
+const inputPokemon = document.querySelector("#pokemon")
+const resultado = document.querySelector("#resultado")
+inputPokemon.addEventListener("keydown", (e) => {
+    if (e.key === "Enter"){
+        buscarPokemon()
+    }
+}
+)
+const buscarPokemon = async () => {
+    const nome = inputPokemon.value
+
+    try {
+        const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`)
+        const dados = await resposta.json()
+
+        resultado.innerHTML = `
+        <h2>${dados.name.toUpperCase()}</h2>
+        <img src = "${dados.sprites.front_default}">
+        <p>Peso: ${dados.weight}</p>
+        <p>Altura: ${dados.height}</p>
+        `
+    }
+    catch (erro) {
+        resultado.innerHTML = "Pokémon não encontrado!"
+    }
+}
